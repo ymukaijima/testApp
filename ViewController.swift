d5784ef75d22a927221ffcef0ddd0aea15802295
k_kita <ak2205d@icloud.com>
2020-07-11 18:09:33 +0900

    commit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestSiri
+//
+//  Created by kita kensuke on 2020/07/11.
+//  Copyright © 2020 kita kensuke. All rights reserved.
+//
+
+import UIKit
+import Intents
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        INPreferences.requestSiriAuthorization { status in
+//            print("status = \(status.rawValue)")
+//        }
+    }
+
+
+}
+
